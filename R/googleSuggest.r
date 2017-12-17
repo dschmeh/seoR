0a4ecc5295b3a0132c44ec4cc079be84c85942d4
Daniel Schmeh <daniel.schmeh@atript.com>
2017-12-17 11:22:08 +0100

    Create googleSuggest.r

---
+++ R/googleSuggest.r
@@ -0,0 +1,68 @@
+#' Function to retrive data from Google Suggest for a specific keyword
+#'
+#' This function allows you to get the Meta-Description of a specific URL.
+#' @keyword The initial keyword you want to get suggestions for
+#' @language The language you want to get suggestions. Default is en.
+#' googleSuggest()
+
+googleSuggest <-
+  function(keyword,
+           language = "en",
+           walkThrough = FALSE) {
+    require(magrittr)
+    require(rvest)
+    #Language Input check
+    if (nchar(language) > 2) {
+      warning("Please check your language input")
+    }
+    keyword <- gsub(" ", "+", keyword)
+    key <- read_html(
+      paste0(
+        "http://suggestqueries.google.com/complete/search?output=toolbar&hl=",
+        language,
+        "&q=",
+        keyword
+      )
+    )
+    sug <- xml_find_all(key, "//suggestion")
+    sug <- gsub('<suggestion data="', "", sug)
+    sug <- gsub('"></suggestion>', "", sug)
+    sug <- as.data.frame(sug)
+    colnames(sug) <- "keyword_suggestions"
+    # Walk through the alphabte for the given keyword (maybe add numbers)
+    if (walkThrough == TRUE) {
+      alphabet <- as.data.frame(c(letters))
+      for (i in 1:nrow(alphabet)) {
+        #query with alphabte after keyword
+        key <- read_html(
+          paste0(
+            "http://suggestqueries.google.com/complete/search?output=toolbar&hl=",
+            language,
+            "&q=",
+            paste0(keyword, "+", alphabet[i, 1])
+          )
+        )
+        s <- xml_find_all(key, "//suggestion")
+        s <- gsub('<suggestion data="', "", s)
+        s <- gsub('"></suggestion>', "", s)
+        s <- as.data.frame(s)
+        colnames(s) <- "keyword_suggestions"
+        #query with alphabte after keyword
+        key <- read_html(
+          paste0(
+            "http://suggestqueries.google.com/complete/search?output=toolbar&hl=",
+            language,
+            "&q=",
+            paste0(alphabet[i, 1], "+", keyword, "+")
+          )
+        )
+        ss <- xml_find_all(key, "//suggestion")
+        ss <- gsub('<suggestion data="', "", ss)
+        ss <- gsub('"></suggestion>', "", ss)
+        ss <- as.data.frame(ss)
+        colnames(ss) <- "keyword_suggestions"
+        sug <- rbind(sug, s, ss)
+      }
+    }
+    return(unique(sug))
+  }
