1ab509017ce30f48a354c51282f0188b3936de5d
Daniel Schmeh <daniel.schmeh@atript.com>
2017-12-23 15:00:55 +0100

    Update linkCount.R

--- R/linkCount.R
+++ R/linkCount.R
@@ -2,22 +2,52 @@
 #'
 #' This function allows you to get the number of outgoing links for a given URL.You can specify if you want to count same Links as one or not.
 #' @url The url you want to get the number of outgoing links for
+#' @linkType The LinkType you want to analyze. All Links, Just Internal Links or Links to other Domains (external). You can specify "all","external" and "internal". Default is "all"
 #' @uniqueLinks Logical. Count unique Links as one is TRUE. Default is FALSE
 #' linkCount()
 
-
-
-linkCount <- function(url, uniqueLinks = FALSE) {
+linkCount <- function(url,
+                      linkType = "all",
+                      uniqueLinks = FALSE) {
   require(magrittr)
   require(rvest)
+  require(stringr)
+  domain <-
+    stringr::str_extract(
+      url,
+      "http(s)?\\:\\/\\/([a-zA-Z0-9-]*\\.)?[a-zA-Z0-9-]*\\.[a-zA-Z0-9]{2,3}(\\.[a-zA-Z0-9]{2,3})?"
+    )
   links <- try(as.data.frame(url %>%
                                as.character() %>%
                                read_html() %>%
                                html_nodes("a") %>%
                                html_attr("href")))
+  colnames(links)[1] <- "links"
   if (uniqueLinks == FALSE)	{
-    return(nrow(links))
+    #Starting with not uniue Links
+    if (linkType == "external") {
+      return(nrow(links) - length(grepl(domain, links$links)[grepl(domain, links$links) ==
+                                                               TRUE]))
+    } else {
+      if (linkType == "internal") {
+        return(length(grepl(domain, links$links)[grepl(domain, links$links) == TRUE]))
+      } else {
+        return(nrow(links))
+      }
+    }
   } else {
-    return(nrow(unique(links)))
+    if (linkType == "external") {
+      return(nrow(unique(links)) - length(grepl(domain, unique(links$links))[grepl(domain, unique(links$links)) ==
+                                                                               TRUE]))
+    } else {
+      if (linkType == "internal") {
+        return(length(grepl(domain, unique(
+          links$links
+        ))[grepl(domain, unique(links$links)) == TRUE]))
+      } else {
+        return(nrow(unique(links)))
+      }
+    }
   }
+
 }
