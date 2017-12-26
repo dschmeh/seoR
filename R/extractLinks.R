fd71294f9fd4025d4341bb342bbb0ce6d8e3bcdb
Daniel Schmeh <daniel.schmeh@atript.com>
2017-12-26 20:20:28 +0100

    Added Linktype
    Now it´s possible just to extract external or internal Links. Default is all as before

--- R/extractLinks.R
+++ R/extractLinks.R
@@ -2,23 +2,61 @@
 #'
 #' This function allows you to extract all links of a given URL. You can specify if you just want unique Links or all. The output is a Data Frame.
 #' @url The url you want to extract the Links of
+#' @linkType The LinkType you want to analyze. All Links, Just Internal Links or Links to other Domains (external). You can specify "all","external" and "internal". Default is "all"
 #' @uniqueLinks Logical. Extract unique Links as one is TRUE. Default is FALSE
 #' extractLinks()
 
 
 
-extractLinks <- function(url, uniqueLinks = FALSE) {
-  require(magrittr)
-  require(rvest)
-  links <- try(as.data.frame(url %>%
-                               as.character() %>%
-                               read_html() %>%
-                               html_nodes("a") %>%
-                               html_attr("href")))
-  colnames(links)[1] <- "links"
-  if (uniqueLinks == FALSE)	{
-    return(links)
-  } else {
-    return(unique(links))
+extractLinks <-
+  function(url,
+           linkType = 'all',
+           uniqueLinks = FALSE) {
+    require(magrittr)
+    require(rvest)
+    require(stringr)
+
+    domain <-
+      stringr::str_extract(
+        url,
+        "http(s)?\\:\\/\\/([a-zA-Z0-9-]*\\.)?[a-zA-Z0-9-]*\\.[a-zA-Z0-9]{2,3}(\\.[a-zA-Z0-9]{2,3})?"
+      )
+    check <- paste0("(", domain, "|^\\/.*)")
+
+
+    links <- try(as.data.frame(url %>%
+                                 as.character() %>%
+                                 read_html() %>%
+                                 html_nodes("a") %>%
+                                 html_attr("href")))
+    colnames(links)[1] <- "links"
+    if (uniqueLinks == FALSE)	{
+      if (linkType == 'internal') {
+        links$check <- as.data.frame(grepl(check, links$links))
+        links <- subset(links, check == TRUE)
+        return(as.data.frame((links$links)))
+      } else {
+        if (linkType == 'external') {
+          links$check <- as.data.frame(grepl(check, links$links))
+          links <- subset(links, check == FALSE)
+          return(as.data.frame((links$links)))
+        } else {
+          return(links)
+        }
+      }
+    } else {
+      if (linkType == 'internal') {
+        links$check <- as.data.frame(grepl(check, links$links))
+        links <- subset(links, check == TRUE)
+        return(as.data.frame(unique(links$links)))
+      } else {
+        if (linkType == 'external') {
+          links$check <- as.data.frame(grepl(check, links$links))
+          links <- subset(links, check == FALSE)
+          return(as.data.frame(unique(links$links)))
+        } else {
+          return(unique(links))
+        }
+      }
+    }
   }
-}
