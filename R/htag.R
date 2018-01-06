29441e408979963c7789ba0a698a6d8c8abc3758
Daniel Schmeh <daniel.schmeh@atript.com>
2018-01-06 20:45:16 +0100

    Update htag.R

--- R/htag.R
+++ R/htag.R
@@ -8,8 +8,6 @@
 
 
 htag <- function(url, hTag = "h1") {
-  requireNamespace(magrittr)
-  requireNamespace(rvest)
   htag <- as.data.frame(try(url %>%
                               as.character() %>%
                               read_html() %>%
