0f965b189f5c6c165b614788139d71bd433a997c
Daniel Schmeh <daniel.schmeh@atript.com>
2018-01-06 20:25:36 +0100

    Documentation for input-checks

--- R/input-checks.R
+++ R/input-checks.R
@@ -1,7 +1,10 @@
 #Functions to check the input
 
-#URL Input checks
 
+#' URL Input checks
+#'
+#' @url The URL you want to analyze
+#' url_with_http()
 ##Check if the User provided a correct URL with Protocoll
 url_with_http <- function(url) {
   return(
@@ -12,30 +15,49 @@ url_with_http <- function(url) {
   )
 }
 
-##Check if the User just provided the Hostname and TLD
+
+#' Check if the User just provided the Hostname and TLD
+#'
+#' @url The URL you want to analyze
+#' host_tld()
 host_tld <- function(url) {
   return(grepl("^[a-zA-Z0-9-]*\\.[a-zA-Z]{2,3}(\\.[a-zA-Z]{2,3})?", url))
 }
 
-#Check if the User provided a correct Sitemap.xml
+
+#' Check if the User provided a correct Sitemap.xml
+#'
+#' @sitemap The sitemap you want to analyze
+#' sitemapxml_check()
 sitemapxml_check<-function(sitemap){
   return(grepl("^http(s)?\\:\\/\\/.*xml$", sitemap))
 }
 
 #Other Input checks
 
-##Check h-Tag Input
+#' Check h-Tag Input
+#'
+#' @h_tag The h_tag you want to analyze
+#' htag_input()
 htag_input <- function(h_tag) {
   return(grepl("^h[1-6]{1}", h_tag))
 }
 
-##Check general choices
+#' Check general choices
+#'
+#' @choice The choice you want to analyze
+#' @possible What are good inputs?
+#' choice()
 choice <- function(choice, possible = "") {
   regex <- paste0("^(", possible, ")$")
   return(grepl(regex, choice))
 }
 
-#Check Screaming-Frog Input Files
+
+#' Check Screaming-Frog Input Files
+#'
+#' @input The input you want to analyze
+#' internal_all_file()
 internal_all_file <- function(input) {
   if (ncol(input) != 41) {
 FALSE
