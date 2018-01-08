d16044184b49b462f7c5f90eb23654822ea2167a
Daniel Schmeh <daniel.schmeh@atript.com>
2018-01-08 16:24:04 +0100

    Added functionality

--- R/googleSuggest.R
+++ R/googleSuggest.R
@@ -4,12 +4,18 @@
 #' @keyword The initial keyword you want to get suggestions for
 #' @walkThrough logical Input. If TRUE the function adds every letter before and after the keyword to loop through the Google Autocomplete.
 #' @language The language you want to get suggestions. Default is en.
+#' @questions Logial. Checks for keywords with question-Phrases like "How much is beer"
+#' @prepositions Logial. Checks for keywords with prepositions-Phrases like "beer without alcohol"
+#' @comparisons Logial. Checks keywords with comparison phrase like "beer versus wine"
 #' googleSuggest()
 
 googleSuggest <-
   function(keyword,
            language = "en",
-           walkThrough = FALSE) {
+           walkThrough = FALSE,
+           questions = FALSE,
+           prepositions = FALSE,
+           comparisons = FALSE) {
     #Language Input check
     if (nchar(language) > 2) {
       warning("Please check your language input")
@@ -50,7 +56,7 @@ googleSuggest <-
         s <- gsub('"></suggestion>', "", s)
         s <- as.data.frame(s)
         colnames(s) <- "keyword_suggestions"
-        #query with alphabte after keyword
+        #query with alphabte before keyword
         key <- read_html(
           paste0(
             "http://suggestqueries.google.com/complete/search?output=toolbar&hl=",
@@ -67,5 +73,79 @@ googleSuggest <-
         sug <- rbind(sug, s, ss)
       }
     }
+    
+    #Check the Keywords with question words
+    
+    if (isTRUE(questions)) {
+      file <- paste0('~/seoR/data/questions_', language, '.csv')
+      questions <- readr::read_csv(file,
+                                   col_names = FALSE)
+      for (i in 1:nrow(questions)) {
+        #query with question keyword
+        key <- read_html(
+          paste0(
+            "http://suggestqueries.google.com/complete/search?output=toolbar&hl=",
+            language,
+            "&q=",
+            gsub(" ", "+", paste0(questions[i, 1], "+", keyword))
+          )
+        )
+        s <- xml_find_all(key, "//suggestion")
+        s <- gsub('<suggestion data="', "", s)
+        s <- gsub('"></suggestion>', "", s)
+        s <- as.data.frame(s)
+        colnames(s) <- "keyword_suggestions"
+        sug <- rbind(sug, s)
+      }
+    }
+    
+    #Check the Keywords with prepositions words
+    
+    if (isTRUE(prepositions)) {
+      file <- paste0('~/seoR/data/prepositions_', language, '.csv')
+      prepositions <- readr::read_csv(file)
+      for (i in 1:nrow(prepositions)) {
+        #query with prepositions keyword
+        key <- read_html(
+          paste0(
+            "http://suggestqueries.google.com/complete/search?output=toolbar&hl=",
+            language,
+            "&q=",
+            gsub(" ", "+", paste0(keyword, "+", prepositions[i, 1]))
+          )
+        )
+        s <- xml_find_all(key, "//suggestion")
+        s <- gsub('<suggestion data="', "", s)
+        s <- gsub('"></suggestion>', "", s)
+        s <- as.data.frame(s)
+        colnames(s) <- "keyword_suggestions"
+        sug <- rbind(sug, s)
+      }
+    }
+    
+    #Check the Keywords with comparisons words
+    
+    if (isTRUE(comparisons)) {
+      file <- paste0('~/seoR/data/comparisons_', language, '.csv')
+      comparisons <- readr::read_csv(file)
+      for (i in 1:nrow(comparisons)) {
+        #query with comparisons keyword
+        key <- read_html(
+          paste0(
+            "http://suggestqueries.google.com/complete/search?output=toolbar&hl=",
+            language,
+            "&q=",
+            gsub(" ", "+", paste0(keyword, "+", comparisons[i, 1]))
+          )
+        )
+        s <- xml_find_all(key, "//suggestion")
+        s <- gsub('<suggestion data="', "", s)
+        s <- gsub('"></suggestion>', "", s)
+        s <- as.data.frame(s)
+        colnames(s) <- "keyword_suggestions"
+        sug <- rbind(sug, s)
+      }
+    }
+    
     return(unique(sug))
   }
